32fcda84143ea9577221d184a1e800a838972e6b
milindmunde <mmunde627@gmail.com>
2020-03-02 22:39:06 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: "AIzaSyDdUFNsabVqhUTcM9LZihk21R3y4SAMy54",
     authDomain: "milindcrud.firebaseio.com",
